990e746c258b5a2885f8454aa07b5bf70c4acb0f
Fin <128983509+FinLemonKe@users.noreply.github.com>
2023-03-27 17:48:56 +0800

    Delete src/main directory

--- src/main/java/me/lemonke/crystalleader/utils/Message.java
+++
@@ -1,9 +0,0 @@
-package me.lemonke.crystalleader.utils;
-
-import org.bukkit.ChatColor;
-
-public class Message {
-    public static String color(String msg) {
-        return ChatColor.translateAlternateColorCodes('&',msg);
-    }
-}
